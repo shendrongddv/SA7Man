8a472d64c9aaf0f3013f3266bf6dff6a1f34c013
shendrongddv <shendrongddv@gmail.com>
2022-12-02 15:25:31 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardContainer = document.querySelector("[data-user-cards-container]");
+
+fetch("users.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((user) => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+
+      header.textContent = user.name;
+      body.textContent = user.email;
+
+      userCardContainer.append(card);
+    });
+  });
